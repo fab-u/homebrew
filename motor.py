3e10f4a1a1c524c0e464a86408eced0136c354d9
Fabian Buehlmann <fabianbuehlmann2000@gmail.com>
2019-11-13 09:40:24 +0100

    Fix errors in motor util

--- motor.py
+++ motor.py
@@ -2,15 +2,14 @@ import RPi.GPIO as GPIO
 class motor:
     def __init__(self, frequencie):
         self.frequencie = frequencie
-        self.dutyCycle = dutyCycle
         GPIO.setup(13, GPIO.OUT)
         self.pwm = GPIO.PWM(13,frequencie)
 
-    def start(speed):
-        pwm.start(speed)
+    def start(self, speed):
+        self.pwm.start(speed)
         
-    def stop():
-        pwm.stop()
+    def stop(self):
+        self.pwm.stop()
 
-    def setSpeed(speed):
-        pwm.ChangeDutyCycle(speed)
+    def setSpeed(self, speed):
+        self.pwm.ChangeDutyCycle(speed)
