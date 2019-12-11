453c9e5817584629f58328fb1c85d504831be818
Fabian Buehlmann <fabianbuehlmann2000@gmail.com>
2019-12-11 14:27:54 +0100

    clean code

--- files.py
+++ files.py
@@ -3,17 +3,10 @@ import os
 
 def _pathBuilder(relative):
     localDir = os.path.dirname(__file__)
-    path = os.path.join(loaclDir, relative)
+    path = os.path.join(localDir, relative)
     return path
 
 
 def getBrewPlans():
     f = open(_pathBuilder('../homebrewPlan/plan.txt'),'r')
     content = f.read()
-
-
-
-#script_dir = os.path.dirname(__file__)
-#file_path = os.path.join(script_dir, './output03.txt')
-#print(file_path)
-#fptr = open(file_path, 'w')
