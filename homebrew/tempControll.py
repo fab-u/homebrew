4d0c8ae00347d860eb77c66535971febe749aad1
Fabian Buehlmann <fabianbuehlmann2000@gmail.com>
2019-12-18 14:42:18 +0100

    add histeresis

--- homebrew/tempControll.py
+++ homebrew/tempControll.py
@@ -1,17 +1,20 @@
 import RPi.GPIO as GPIO
 
-tempSet = 0
+tempSet = 0 #Set Temperature in °C
+hysteresis = 1 # hysteresis in °C
 
-gpio = 13
+gpio = 13 #Raspi GPIO pin 
 GPIO.setup(gpio, GPIO.OUT)
 
+#Set Temperature 
 def setTemp(temp):
     tempSet = temp
 
+#Update heat plate with current temperature values
 def update(tempActual):
-    if(tempActual>tempSet):
+    if(tempActual> (TempSet + hysteresis)):
         GPIO.output(gpio, GPIO.HIGH)
-    else:
+    else if(tempActual> (TempSet - hysteresis))):
         GPIO.output(gpio, GPIO.LOW)
 
 def off():
