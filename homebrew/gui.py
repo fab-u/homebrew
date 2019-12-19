c00c4c99d1c46d7fc7a42899d3284b4775f8cd1f
Fabian Buehlmann <fabianbuehlmann2000@gmail.com>
2019-12-19 21:29:12 +0100

    add general gui loader

---
+++ homebrew/gui.py
@@ -0,0 +1,17 @@
+import sys
+import gui_auto, gui_man
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+app = QtWidgets.QApplication(sys.argv)
+app.setStyle('Windows')
+MainWindow = QtWidgets.QMainWindow()
+
+def load(isAuto):
+    if isAuto:
+        ui = gui_auto.Ui_MainWindow()
+    else:
+        ui = gui_man.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    app.exec_()
