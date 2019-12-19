2479676759e3caeb0c05e99bb4516373582e9e2c
Fabian Buehlmann <fabianbuehlmann2000@gmail.com>
2019-12-19 21:29:51 +0100

    add main loop

--- homebrew/main.py
+++ homebrew/main.py
@@ -1,15 +1,35 @@
 #raspi lib
 import RPi.GPIO as GPIO
 
-#General GPIO Settings
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-
-#User lib
+#user lib
 import motor
 import gpio
 import program
-import tempControll
+import tempControl
 import tempSens
+import gui, gui_auto, gui_man
+
+#local variables
+isOn = False
+_isAuto = False
+
+#general GPIO Settings
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
 
 #initialize system
+gui.load(False) #load manual user interface
+
+#main loop
+while True:
+    if _isAuto:
+        if isOn:
+            program.update()
+    if(isOn):
+        tempControl.update(tempSens.get())
+    else:
+        tempControl.off()
+
+def setMode(isAuto):
+    _isAuto = isAuto
+    gui.load(isAuto)
